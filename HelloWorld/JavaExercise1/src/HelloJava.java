115098c5ce6b107d98a58617dcd06c90b06401fa
jamuna555 <jamunashree.jb@gmail.com>
2015-06-24 17:26:27 +0530

    Create HelloJava.java

---
+++ HelloWorld/JavaExercise1/src/HelloJava.java
@@ -0,0 +1,19 @@
+
+public class HelloJava {
+
+		public static void main(String[] args) 
+		{
+			System.out.println("\n***************************************");
+			System.out.println("\n\t\tJava Features\t\t");
+			System.out.println("\n***************************************");
+			System.out.println("\n1.\"WORA\"- Write once Run AnyWhere");
+			System.out.println("\n2. Object Oriented Programming Language");
+			System.out.println("\n3. Its very simple Language");
+			System.out.println("\n4. Multithreading ");
+			System.out.println("\n5. Pre-Defined classes and functionalities");
+			System.out.println("\n6. Exception Handling");
+			System.out.println("\n7. Platform Independent");
+			
+	    }
+
+}
